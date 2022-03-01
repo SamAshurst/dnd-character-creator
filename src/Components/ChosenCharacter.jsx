38c2a9ccdff665f30082a47791f711d7a58b6e5f
BillyZen <willjayandtay@hotmail.co.uk>
2022-03-01 12:33:33 +0000

    chosen character component

--- src/Components/ChosenCharacter.jsx
+++ src/Components/ChosenCharacter.jsx
@@ -1,14 +1,16 @@
 export default function ({character, classData}) {
+    console.log(classData)
     if (character.class === "" || classData.name === ''){
         return <p>Select your Character!</p>
     }
-    return <section>
+    return (
+    <section className="character">
+        <h2>{`You are a ${character.race} ${character.class}`}</h2>
         <div className={character.race}>
             <div className="character-text-container">
-                <h2>{`You are a ${character.race} ${character.class}`}</h2>
-                <p>{`Prepare your ${classData.proficiencies[0].name} and ${classData.proficiencies[1].name}`}</p>
-
+                <p>{`Prepare your ${character.class === "Fighter" ? "fists" : classData.starting_equipment[0].equipment.index}, ${character.class} and get ready to battle monsters in order to save all ${character.race}-kind!`}</p>
             </div>
         </div>
     </section>
+    )
 }
